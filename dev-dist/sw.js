7f35c0157f0ad6e950d349dadec10951a794c5ad
Danny Phillips <danny.phillips8@gmail.com>
2025-01-10 10:47:38 -0800

    setup auth

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h515g2ibk2"
+    "revision": "0.go2pig8r45o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
