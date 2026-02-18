c585d9ea1a8cba526f016a45daafe764cdbefba0
Danny Phillips <danny.phillips8@gmail.com>
2026-02-17 17:38:44 -0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/phillips-academy/sw.js', { scope: '/phillips-academy/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
